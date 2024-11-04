9de48801c1f20f0bddc5cb2d02ef3c7d59463b8d
yanis.hlali <yanis.hlali@gmail.com>
2024-11-04 11:58:56 +0100

    Refactor database connection to use connection pool for improved performance

--- database.js
+++ database.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à MySQL:', err.message);
-  } else {
-    console.log('Connecté à la base de données MySQL.');
-  }
-});
-
-module.exports = db;
+module.exports = pool.promise();
